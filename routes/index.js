a461f87165deb01eaa433bc7fb5bfc69f3f97c3b
Leonardo <leo_botrel@hotmail.com>
2024-03-31 12:42:38 -0300

    get bd connection

--- routes/index.js
+++ routes/index.js
@@ -4,6 +4,7 @@ const mysql = require('mysql2/promise');
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
+  res.render('index', {title: 'Lojinha'});
   mysql.createConnection({host: 'localhost', user: 'aula_express', password: 'P@ssw0rd', database: 'aula_express', port: '3306'})
   .then((connection) => {connection.query('SELECT * FROM produtos;')
     .then((result) => {res.send(result[0]);});
