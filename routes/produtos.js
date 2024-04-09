b510a283dabcf587e75dff655971124080133730
Leonardo <leo_botrel@hotmail.com>
2024-04-08 23:30:03 -0300

    final

--- routes/produtos.js
+++ routes/produtos.js
@@ -1,6 +1,5 @@
 const express = require('express');
 const router = express.Router();
-// const mysql = require('mysql2/promise');
 
 const produtoConstroller = require('../controllers/produtoController');
 const nomeMiddleware = require('../middlewares/nomeMiddleware');
@@ -9,13 +8,10 @@ const valorMiddleware = require('../middlewares/valorMiddleware');
 
 /* GET produtos. */
 router.get('/', produtoConstroller.findAll);
-
+/* POST produtos. */
+router.post('/', valorMiddleware.validatePreco, nomeMiddleware.validateNome, produtoConstroller.save);
 /* PUT produtos. */
 router.put('/', valorMiddleware.validatePreco, nomeMiddleware.validateNome, produtoConstroller.update);
-
-/* POST produtos. */
-router.post('/', produtoConstroller.save);
-
 /* DELETE produtos. */
 router.delete('/:id', produtoConstroller.remove);
 
--- services/produtoService.js
+++ services/produtoService.js
@@ -8,22 +8,22 @@ const findAll = async () => {
 
 const update = async (produto) => {
     const query = 'UPDATE produtos SET nome = ?, descricao = ?, preco = ? WHERE id = ?';
-    const isOK = await (await connection).execute(query,
-        [produto.nome, produto.descricao, produto.preco, produto.id]);
+    const isOK = await (await connection)
+        .execute(query, [produto.nome, produto.descricao, produto.preco, produto.id]);
     return isOK[0].affectedRows === 1;
 };
 
 const save = async (produto) => {
     const query = 'INSERT INTO produtos(nome, descricao, preco) VALUES (?, ?, ?)';
-    const isOK = await (await connection).execute(query,
-        [produto.nome, produto.descricao, produto.preco, produto.id]);
+    const isOK = await (await connection)
+        .execute(query, [produto.nome, produto.descricao, produto.preco, produto.id]);
     return isOK[0].affectedRows === 1;
 };
 
 const remove = async (id) => {
     const query = 'DELETE FROM produtos WHERE id = ?';
-    const isOK = await (await connection).execute(query,
-        [id]);
+    const isOK = await (await connection)
+        .execute(query, [id]);
     return isOK[0].affectedRows === 1;
 };
 
